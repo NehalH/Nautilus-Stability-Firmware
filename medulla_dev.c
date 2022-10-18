f6ab857f96178005cf82bc4b7270a6e75b0ce2fd
proffesCode <102415362+proffesCode@users.noreply.github.com>
2022-10-18 10:36:41 +0530

    fixed ur code
    .

--- medulla_dev.c
+++ medulla_dev.c
@@ -1,4 +1,4 @@
-include <Stepper.h>
+#include <Stepper.h>
 #include <Wire.h>
 #include <Servo.h>
 
@@ -199,7 +199,7 @@ void loop() {
     
     // Save previous x reading
  
-    prevX = x
+    prevX = x;
     
  /////////////////////////////////////////////////////////////////    Thrust vectoring for pitch stability
  
